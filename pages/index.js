5736b2da80ec010dfb1b7448e29bd0eb99f05a51
Benjamin Shafii <ben@prologe.io>
2022-04-23 18:01:06 +0000

    fix: button

--- pages/index.js
+++ pages/index.js
@@ -211,7 +211,6 @@ const CreatorForm = () => {
     <CardSelect label={'Body Collections'} />
     <button
       type="submit"
-      type="button"
       className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
     >
       Create
