50f36aa8ba6511d1425dd34f070ead6b5aacca63
Laurent De Marez <laurent@daonative.xyz>
2022-04-23 19:24:41 +0200

    feat: add erase button

--- pages/draw.js
+++ pages/draw.js
@@ -97,6 +97,11 @@ export default function Draw() {
 
           <div className="flex flex-col gap-8 items-center">
             <CirclePicker colors={COLORS} color={selectedColor} onChangeComplete={changeColor} />
+            <button onClick={() => changeColor({hex: "#ffffff00"})}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
             <DrawingPanel selectedColor={selectedColor} />
           </div>
         </div>
