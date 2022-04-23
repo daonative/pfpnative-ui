1a36f2e970bb97194810a184bca2fc4bc5441557
Benjamin Shafii <ben@prologe.io>
2022-04-23 21:43:29 +0000

    feat: update button

--- pages/index.js
+++ pages/index.js
@@ -213,7 +213,7 @@ const CreatorForm = () => {
       <CardSelect label={'Body Collections'} />
       <button
         type="submit"
-        className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
         Create
       </button>
